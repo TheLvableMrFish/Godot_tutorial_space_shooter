c1cbe5b87a7c244ff699f7e5d39b5938676ef0d1
TheLvableMrFish <paganochristians@gmail.com>
2025-05-29 21:52:10 -0500

    bug popped up from deleting level, went into file and deleted what seemed to be a reference, might need to restart tutorial

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+# 1. Load the scene
+var meteor_scene: PackedScene = ("res://scenes/meteor.tscn")
+
+func _on_meteor_timer_timeout():
+	# 2. create an instance
+	var meteor = meteor_scene.instance()
+	
+	#  3. Attach the node to the level scene tree
+	$Meteors.add_child(meteor)
+	
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,21 +1,21 @@
 extends CharacterBody2D
 
 @export var SPEED: int = 500
-# SPEED := 500 shorthand
+
+signal laser
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(100, 500)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+	print("works??????")
 	var direction = Input.get_vector("left", "right", "up", "down")
-	##get_node("PlayerImage").rotation += 0.1 * delta
-	##$PlayerImage.rotation += 0.2 * delta
-	#position += direction * SPEED * delta 
 	velocity = direction * SPEED
 	move_and_slide()
+	
+	# Shoot input
+	#laser.emit()
 	
 	
